06fd647a1c96a1193311a568b02c7cf00b2c8f0e
lopez824 <lionelslopez14@gmail.com>
2021-08-26 15:37:55 -0600

    Removed extra files from my branch

--- TiltDefense/TiltDefense/Game1.cs
+++ TiltDefense/TiltDefense/Game1.cs
@@ -11,8 +11,6 @@ namespace TiltDefense
         private GraphicsDeviceManager _graphics;
         private AndroidGyro gyro;       // class to hand android gyroscope
         private SpriteBatch _spriteBatch;
-        private Texture2D pikaTexture;
-        private Vector2 pikaPosition;
 
         // Set speed delay for monitoring changes
         private SensorSpeed sensorSpeed = SensorSpeed.UI;
@@ -20,7 +18,7 @@ namespace TiltDefense
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
-            gyro = new AndroidGyro();
+            
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -28,7 +26,8 @@ namespace TiltDefense
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            pikaPosition = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, _graphics.GraphicsDevice.Viewport.Width / 2);
+            
+            gyro = new AndroidGyro();
             gyro.Init();
             base.Initialize();
         }
@@ -38,7 +37,7 @@ namespace TiltDefense
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            pikaTexture = Content.Load<Texture2D>("pikachu");
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -56,19 +55,6 @@ namespace TiltDefense
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(
-                pikaTexture,
-                pikaPosition,
-                null,
-                Color.White,
-                0f,
-                new Vector2(pikaTexture.Width / 2, pikaTexture.Height / 2),
-                Vector2.One,
-                SpriteEffects.None,
-                0f
-            );
-            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- TiltDefense/TiltDefense/obj/Debug/100/android/environment.x86_64.s
+++ TiltDefense/TiltDefense/obj/Debug/100/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"32465d6e-2bde-49fd-9427-102910152567"
+	.asciz	"05a2580e-0489-4818-ae27-06af9c001900"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
