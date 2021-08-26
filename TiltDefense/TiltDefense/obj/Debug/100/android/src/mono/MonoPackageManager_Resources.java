63cb7fea4af6ea77898a65ae3150ff0242f18064
lopez824 <lionelslopez14@gmail.com>
2021-08-26 12:44:06 -0600

    Updated Project Properties for Android

---
+++ TiltDefense/TiltDefense/obj/Debug/100/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,10 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "TiltDefense.dll" comes first in this list. */
+		"TiltDefense.dll",
+		"MonoGame.Framework.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
